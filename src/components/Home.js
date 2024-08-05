a63f03dfd1918e2a518e47620f1876b7d6a2174a
Purity Wambua <puritywamb@gmail.com>
2024-08-05 10:19:50 +0300

    react hooks jsx

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,15 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
+  return (
+    <div id="home">
+      <h1 style={{ color: 'firebrick' }}>
+        {name} is a Web Developer from {city}
+      </h1>
+    </div>
+  );
   // update the JSX being returned!
-  return <div>Home</div>;
+ 
 }
 
 export default Home;
